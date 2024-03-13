f979ea5f99e69041e60752ec218487ac374e3ec9
PotatoFishes <39881509+PotatoFishes@users.noreply.github.com>
2024-03-13 10:09:43 -0500

    created working logging feature

--- src/background.js
+++ src/background.js
@@ -6,49 +6,163 @@
  * See the Chrome extension documentation for more information:
  * https://developer.chrome.com/docs/extensions/mv3/background_pages/
  */
-
 // Constants
 const ACTION_DESCRIBE_IMAGE = 'DESCRIBE_IMAGE';
-const SERVER_URL = 'http://localhost:8000';
-const SERVER_DESCRIBE_PATH = "/describe";
+const ACTION_LOGIN = 'LOGIN';
+const ACTION_LOG = 'LOG';
+const ACTION_ERROR = 'ERROR';
+const SERVER_URL = 'https://i2tcapstone.azurewebsites.net';
+const SERVER_DESCRIBE_PATH = "/i2t/describe";
+const SERVER_LOGIN_PATH = "/users/login"
+const SERVER_LOG = "/logs"
+let resolveToken = () => {};
+const TOKEN = new Promise((resolve) => resolveToken = resolve);
 
-// Listen for messages from the content script
-chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
-    if (request.action === ACTION_DESCRIBE_IMAGE) {
-        console.debug('Received request to describe image:', request.url)
+// Gets access token from storage
+function getToken() {
+    return chrome.storage.sync.get("token").then((response) => {
+        if (response.value) {
+            return response.value;
+        } else {
+            return TOKEN;
+        }
+    })
+}
+
+// Listen for login button
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.name === ACTION_LOGIN) {
+        // Acquire access token
+        login(message.username, message.password);
+    } else if (message.name === ACTION_LOG) {
+        // Logs message
+        log(message.msg);
+    } else if (message.name === ACTION_ERROR) {
+        // Error logs
+        logError(message.msg, message.stack);
+    } else if (message.action === ACTION_DESCRIBE_IMAGE) {
+        describe(message, sendResponse);
+    }
+    return true; // Required to keep the message channel open while waiting for the response
+
+})
+
+// requests server to describe
+function describe(message, sendResponse) {
+    getToken().then(token => {
+        // Listen for messages from the content script
+        log("Received request to describe image: " + message.url)
 
         // Send the image URL to the server to be described
         fetch(`${SERVER_URL}${SERVER_DESCRIBE_PATH}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
             },
-            body: JSON.stringify({ url: request.url }),
+            body: JSON.stringify({ url: message.url }),
         })
 
-        // Check if the server responded with an error
-        .then(response => {
-            if (!response.ok) {
-                console.error(`Server responded with ${response.status} ${response.statusText}`)
-                if (response.status === 422) {
-                    throw new Error('Invalid image URL: ' + request.url);
+            // Check if the server responded with an error
+            .then(response => {
+                if (!response.ok) {
+                    console.error(`Server responded with ${response.status} ${response.statusText} for image ${message.url}`)
+                    if (response.status === 422) {
+                        logError("Invalid image URL: " + message.url)
+                        throw new Error('Invalid image URL: ' + message.url);
+                    }
+                    throw new Error('Server failed to describe the image');
                 }
-                throw new Error('Server failed to describe the image');
-            }
-            return response;
-        // Parse the response as JSON
-        }).then(response => {
+                return response;
+                // Parse the response as JSON
+            }).then(response => {
             return response.json();
         })
-        // Send the description back to the content script
-        .then(data => {
-            sendResponse({ description: data.description });
+            // Send the description back to the content script
+            .then(data => {
+                sendResponse({ description: data.description });
+            })
+            // Handle any errors that occurred during the request
+            .catch(error => {
+                logError("Failed to get image description: " + error)
+                sendResponse({ description: error.message });
+            });
+    })
+}
+
+// Gets access token
+function login(user, pass) {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({username: user, password: pass})
+    };
+
+    fetch(`${SERVER_URL}${SERVER_LOGIN_PATH}`, options)
+        .then(response => response.json())
+        .then(response => {
+            resolveToken(response.access_token);
+            chrome.storage.sync.set({token: {value: response.access_token}});
         })
-        // Handle any errors that occurred during the request
-        .catch(error => {
-            console.error('Failed to get image description:', error);
-            sendResponse({ description: error.message });
-        });
-    }
-    return true; // Required to keep the message channel open while waiting for the response
-});
+        .catch(err => console.error(err));
+}
+
+function log(msg) {
+    console.log(msg);
+    getToken().then(token => {
+        const logMessage = {
+            message: msg,
+            level: {label: "info", value: 2},
+            logger: "client",
+            timestamp: new Date().toISOString(),
+            stacktrace: ""
+        }
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                logs: [logMessage],
+                count: 1
+            })
+        };
+
+        fetch(`${SERVER_URL}${SERVER_LOG}`, options)
+            .catch(err => console.error(err));
+    })
+}
+
+// Error logging function
+function logError(msg, sta) {
+    console.error(msg, sta);
+    getToken().then(token => {
+        const stack = sta ?? new Error().stack;
+        const logMessage = {
+            message: msg,
+            level: {label: "error", value: 4},
+            logger: "client",
+            timestamp: new Date().toISOString(),
+            stacktrace: stack
+        }
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                logs: [logMessage],
+                count: 1
+            })
+        };
+
+        fetch(`${SERVER_URL}${SERVER_LOG}`, options)
+            .catch(err => console.error(err));
+    })
+}
--- src/content.js
+++ src/content.js
@@ -9,6 +9,38 @@
 
 // Constants
 const ACTION_DESCRIBE_IMAGE = 'DESCRIBE_IMAGE';
+const ACTION_LOG = 'LOG';
+const ACTION_ERROR = 'ERROR';
+
+
+// Feedback logging mechanism
+function logImageDetails() {
+    const selectedImage = window.getSelection().anchorNode;
+    if (selectedImage && selectedImage.tagName === 'IMG') {
+        const url = selectedImage.src;
+        const altText = selectedImage.alt;
+        const textContent = 'good';
+        console.log(`URL: ${url}, Alt Text: ${altText}, Text Content: ${textContent}`);
+    } else {
+        console.log('No image selected or not an image element.');
+    }
+}
+logImageDetails();
+
+
+// logs messages
+function log(message) {
+    console.log(message);
+    chrome.runtime.sendMessage({name: ACTION_LOG, msg: message});
+}
+
+
+// logs errors
+function logError(message) {
+    const sta = new Error().stack;
+    chrome.runtime.sendMessage({name: ACTION_ERROR, msg: message, stack: sta})
+}
+
 
 // This function will be executed on each image once it is loaded
 const sendImageToServiceWorker = (image) => {
@@ -16,25 +48,29 @@ const sendImageToServiceWorker = (image) => {
     //       captioning model (see TF project). The service worker will reply with a message,
     //       and the content script will update the image caption of the corresponding URL
     //       (or id). If using id, then we will need to set the id first!
-
     // For now, we will just send the image URL to the service worker
     chrome.runtime.sendMessage({ action: ACTION_DESCRIBE_IMAGE, url: image.src }, (response) => {
         console.debug('Received response from service worker:', response);
         // Update the image caption with the response from the service worker
         image.alt = response.description;
+        log("URL, text: " + image.url + " " + image.alt)
     });
 };
+
 
 // Function to add a 'load' event listener to an image
 const addLoadListenerToImage = (image) => {
     // Check if the image is already loaded (important for cached images)
     if (image.complete) {
+        log("sent image to service worker: " + image)
         sendImageToServiceWorker(image);
     } else {
         // If the image is not yet loaded, listen for the load event
+        log("add load listener for image: " + image)
         image.addEventListener('load', () => sendImageToServiceWorker(image));
     }
 };
+
 
 // Observe the DOM for added images
 const observeDOMChanges = () => {
@@ -43,6 +79,7 @@ const observeDOMChanges = () => {
             mutation.addedNodes.forEach((node) => {
                 // Check if the added node is an image
                 if (node.tagName === 'IMG') {
+                    log("Mutation Observer updating")
                     addLoadListenerToImage(node);
                 }
                 // If the node has children, look for images within it
@@ -61,8 +98,10 @@ const observeDOMChanges = () => {
     observer.observe(document.body, config);
 };
 
+
 // Add event listeners to all images currently in the DOM
 const initialize = () => {
+    log("Chrome plugin initialized")
     document.querySelectorAll('img').forEach(addLoadListenerToImage);
     observeDOMChanges();
 };
