57fcf9e7452bffc1ce2a10b05c8f93c58094a1cd
Daniel Luper <jdaniel.luper@gmail.com>
2024-04-04 18:01:52 -0500

    collect hardware info

--- src/background.js
+++ src/background.js
@@ -150,3 +150,39 @@ function logError(msg, sta) {
             .catch(err => console.error(err));
     })
 }
+
+
+const logSystemInfo = async () => {
+    let systemInfo = {};
+
+    const cpuInfoPromise = new Promise((resolve) => {
+        chrome.system.cpu.getInfo((info) => {
+            systemInfo.cpuArch = info.archName;
+            systemInfo.cpuModel = info.modelName;
+            systemInfo.cpuNumOfProcessors = info.numOfProcessors;
+            resolve();
+        });
+    });
+
+    const memoryInfoPromise = new Promise((resolve) => {
+        chrome.system.memory.getInfo((info) => {
+            systemInfo.memoryCapacityGB = info.capacity / 1024 / 1024 / 1024;
+            resolve();
+        });
+    });
+
+    await Promise.all([cpuInfoPromise, memoryInfoPromise]);
+
+    log(`"systeminfo": ${JSON.stringify(systemInfo, null, 2)}`);
+}
+
+// Send system info to server
+logSystemInfo();
+
+// Log memory usage every 10 seconds
+setInterval(() => {
+    chrome.system.memory.getInfo((info) => {
+        const availableMemoryMB = info.availableCapacity / 1024 / 1024;
+        log(`Available Memory: ${availableMemoryMB.toFixed(2)} MB`);
+    });
+}, 10000);
